460ce89bd9d6bd26606a03418f427c253ce3353d
z.terehov <zaharkuli67@gmail.com>
2023-02-13 22:46:43 +0300

    started doing

---
+++ users/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './index.scss';
+import { Success } from './components/Users';
+import { Users } from './components/Users';
+
+// Тут список пользователей: https://reqres.in/api/users
+
+function App() {
+  const [users, setUsers] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://reqres.in/api/users')
+    .then(res => res.json())
+    .then(json => {
+      setUsers(json.data);
+    })
+    .catch((err) => {
+      console.warn(err);
+      alert('Ошибка при получении пользователей');
+    })
+    .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <div className="App">
+      <Users items={users} isLoading={isLoading}/>
+      {/* <Success /> */}
+    </div>
+  );
+}
+
+export default App;
